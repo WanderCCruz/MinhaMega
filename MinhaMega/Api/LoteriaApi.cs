7030443c36a01ae669dc1d5d2c050f272ff36a36
Wander Cassio da Cruz Almeida <wander.ccruz@gmail.com>
2023-05-08 23:17:43 -0300

    inclusão de url base

---
+++ MinhaMega/Api/LoteriaApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaMega.Api
+{
+    public class LoteriaApi
+    {
+        private const string UrlBase = "https://servicebus2.caixa.gov.br/portaldeloterias/api/";
+    }
+}
