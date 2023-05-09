6232757dca006f7405fd81d7af2e43c920948a83
Wander Cassio da Cruz Almeida <wander.ccruz@gmail.com>
2023-05-08 22:28:13 -0300

    Criação inicial do Projeto

---
+++ MinhaMega/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MinhaMega;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MinhaMega/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MinhaMega;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
