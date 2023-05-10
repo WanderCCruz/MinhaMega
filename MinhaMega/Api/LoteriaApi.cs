580d9c1e679884411d580688b8fc9a2bd210747d
Wander Cassio da Cruz Almeida <wander.ccruz@gmail.com>
2023-05-09 22:41:20 -0300

    Inicio da criação da Api

--- MinhaMega/Api/LoteriaApi.cs
+++ MinhaMega/Api/LoteriaApi.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using System.Reflection.Metadata;
 
 namespace MinhaMega.Api
 {
-    public class LoteriaApi
+    public class LoteriaApi : ILoteriaApi
     {
-        private const string UrlBase = "https://servicebus2.caixa.gov.br/portaldeloterias/api/";
+        private readonly HttpClient httpClient;
+        string urlBase = "https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena/";
+        public LoteriaApi(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<string> Concurso(int numeroConcurso)
+        {
+            httpClient.BaseAddress = new Uri(urlBase+numeroConcurso.ToString());
+            HttpResponseMessage response = await httpClient.GetAsync("");
+            if (response.IsSuccessStatusCode)
+                return response.Content.ReadAsStringAsync().Result;
+            return null;
+        }
     }
 }
