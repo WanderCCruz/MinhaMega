31b6071f7d3da915fd7e2eaf7f2b80e1b148807b
Wander Cassio da Cruz Almeida <wander.ccruz@gmail.com>
2023-05-28 01:02:11 -0300

    Criação de conversor, ajustes no layaut e conversão de valores em moeda, ajustes no layout de tela

--- MinhaMega/ViewModels/HomePageViewModel.cs
+++ MinhaMega/ViewModels/HomePageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Maui.Controls;
+using MinhaMega.Api;
 using MinhaMega.Models;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,12 @@ namespace MinhaMega.ViewModels
     [QueryProperty(nameof(ResultadoMega), nameof(ResultadoMega))]
     public partial class HomePageViewModel : ObservableObject
     {
+        private readonly ILoteriaApi<MegaSena> _api;
+
+        public HomePageViewModel(ILoteriaApi<MegaSena> api)
+        {
+            _api = api;
+        }
 
         [RelayCommand]
         private async Task Voltar()
@@ -21,5 +28,17 @@ namespace MinhaMega.ViewModels
         }
         [ObservableProperty]
         MegaSena resultadoMega;
+
+        [RelayCommand]
+        async Task BuscaConcurso(string concurso)
+        {
+            if (Connectivity.Current.NetworkAccess == NetworkAccess.Internet)
+                ResultadoMega = await _api.Concurso(nameof(MegaSena).ToLower(), (Convert.ToInt32(concurso)));
+            else
+            {
+                await Shell.Current.DisplayAlert("Atenção", "Verifique se sua internet esta funcionando !!!", "OK");
+                return;
+            }
+        }
     }
 }
